897204b7d0fc2de9c48bf219e9e5913a3f1fe2bb
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-05-02 19:47:45 +0300

    Feat: Define handleLabelSelected function

--- src/components/Labels.js
+++ src/components/Labels.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const LabelsApp = () => {
   const [selectedLabel, setSelectedLabel] = React.useState(null);
-
+  
+  const handleLabelSelected = (label) => {
+    setSelectedLabel(label);
   return (
     <div>
     </div>

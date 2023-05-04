5ddb9db4c006e298d4364ab319b111ba866117d2
Victor <victor.ireri@student.moringaschool.com>
2023-05-04 11:01:54 +0300

    Feat completed app.js

---
+++ src/components/TaskDisplay.js
@@ -0,0 +1,44 @@
+
+import React from "react";
+
+export default function TaskDisplay({tasks,handleDeleteClick,handleEditClick}) {
+  if (!tasks) {
+    return <div></div>
+  }
+
+  return (
+    <div>
+      <h3>Task List</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Due Date</th>
+              <th>Priority</th>
+              <th>Completed</th>
+              <th>Assigned to</th>
+              <th>Action</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <tr key={task.id}>
+                <td>{task.title}</td>
+                <td>{task.description}</td>
+                <td>{task.due_date}</td>
+                <td>{task.priority}</td>
+                <td>{task.completed ? 'Yes' : 'No'}</td>
+                <td>{task.assigned_to}</td>
+                <td>
+                  <button onClick={() => handleEditClick(task)}>Edit</button>
+                  <button onClick={() => handleDeleteClick(task.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+    </div>
+  )
+}

608bb8ee9e7f7ae67ac7e5cceb330573b41e506a
kennedy <kimaniken2001@gmail.com>
2023-05-04 10:57:44 +0300

    added TaskDisplay file

---
+++ src/components/Home.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Task from "./Tasks";
+import TaskDisplay from "./TaskDisplay";
+
+function Home({ currentUser }) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/tasks")
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredTasks = data.filter((task) => {
+          return (
+            task.assigned_to &&
+            task.assigned_to.username === currentUser?.username
+          );
+        });
+        setTasks(filteredTasks);
+      });
+  }, [currentUser]);
+
+  const handleTaskCreated = (newTask) => {
+    if (!currentUser) {
+      alert("You need to be logged in to add a task.");
+      return;
+    }
+    setTasks([...tasks, newTask]);
+  };
+
+  return (
+    <div>
+      {currentUser && <h2>Welcome {currentUser.username}</h2>}
+      <Task
+        userId={currentUser?.id}
+        username={currentUser?.username}
+        onTaskCreated={handleTaskCreated}
+        setTasks={setTasks} // pass down setTasks function as a prop
+      />
+      <TaskDisplay/>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login({ users, onLogin, onSignUp, setCurrentUser }) {
+  console.log(setCurrentUser);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const user = findUser(username, password);
+    if (user) {
+      onLogin(user);
+      setCurrentUser(user);
+      navigate('/');
+    } else {
+      setError('Invalid username or password');
+    }
+  };
+
+  const handleSignUpClick = (event) => {
+    event.preventDefault();
+    onSignUp();
+  };
+
+  const findUser = (username, password) => {
+    return users.find(user => user.username === username && user.password === password);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <p>Don't have an account? <a href="/signup" >Sign up</a></p>
+    </div>
+  );
+}
+
+export default Login;
